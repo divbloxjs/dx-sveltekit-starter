f358413b231c01c15442c86391114eaa88a9c168
Dani <dani.simeonov@stratusolve.com>
2024-04-10 15:19:30 +0200

    Added shadcn

--- dx.config.js
+++ dx.config.js
@@ -1,9 +1,9 @@
 // Every property can be overwritten by node ENV variables of the same name
 export default {
-	dxApiKey: 'd405a30d05b130e21bed5b544027672f', // Divblox API key used to access resources stored there
-	environment: 'local', // Current environment,
-	dataModelPath: 'divblox/configs/datamodel.json', // Path from route to the data model JSON file
-	databaseConfigPath: 'divblox/configs/database.config.js', // Path from root to the database configuration file
-	databaseCaseImplementation: 'snakecase', // Allowed options ['snakecase'|'camelcase'|'pascalcase']
-	ormImplementation: 'prisma' // Supported options ['none','prisma']; If defined, the relevant orm will automatically be installed and used for things like component generation
+    dxApiKey: "d405a30d05b130e21bed5b544027672f", // Divblox API key used to access resources stored there
+    environment: "local", // Current environment,
+    dataModelPath: "divblox/configs/datamodel.json", // Path from route to the data model JSON file
+    databaseConfigPath: "divblox/configs/database.config.js", // Path from root to the database configuration file
+    databaseCaseImplementation: "snakecase", // Allowed options ['snakecase'|'camelcase'|'pascalcase']
+    ormImplementation: "prisma" // Supported options ['none','prisma']; If defined, the relevant orm will automatically be installed and used for things like component generation
 };
