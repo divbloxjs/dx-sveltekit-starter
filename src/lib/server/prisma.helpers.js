4222a71285e916a2012cb37d43d9d54c5602adb0
Dani <dani.simeonov@stratusolve.com>
2024-04-12 09:59:40 +0200

    Added prisma helpers and divblox aliases

---
+++ src/lib/server/prisma.helpers.js
@@ -0,0 +1,100 @@
+import dataModel from "datamodel";
+import { isEmptyObject } from "dx-utilities";
+
+export const getPrismaSelectAllFromEntity = (entityName, select = {}) => {
+    if (isEmptyObject(select)) select.id = true;
+
+    // Add all attributes
+    Object.keys(dataModel[entityName].attributes).forEach((attributeName) => {
+        select[attributeName] = true;
+    });
+
+    // Nested add all relationships
+    Object.keys(dataModel[entityName].relationships).forEach((relationshipName) => {
+        select[relationshipName] = { select: {} };
+        getPrismaSelectAllFromEntity(relationshipName, select[relationshipName].select);
+    });
+
+    return select;
+};
+
+export const getPrismaConditions = (entityName = "", searchConfig = {}, constraints = {}) => {
+    const prismaConditions = {};
+
+    if (constraints.limit) {
+        prismaConditions.take = constraints.limit;
+    }
+
+    if (constraints.offset) {
+        prismaConditions.skip = constraints.offset;
+    }
+
+    if (constraints.sort) {
+        prismaConditions.orderBy = [];
+        Object.keys(constraints.sort).forEach((attributeName) => {
+            const obj = {};
+            obj[attributeName] = constraints.sort[attributeName];
+            prismaConditions.orderBy.push(obj);
+        });
+    }
+
+    if (constraints.search) {
+        if (!prismaConditions.where) prismaConditions.where = {};
+
+        searchConfig.attributes.forEach((attributeName) => {
+            if (!prismaConditions.where.OR) prismaConditions.where.OR = [];
+            prismaConditions.where.OR.push({ [attributeName]: { contains: constraints.search } });
+        });
+
+        Object.keys(searchConfig.relationships).forEach((entityName) => {
+            if (!prismaConditions.where.OR) prismaConditions.where.OR = [];
+            const relationshipConstraint = { [entityName]: {} };
+            searchConfig.relationships[entityName].attributes.forEach((attributeName) => {
+                relationshipConstraint[entityName][attributeName] = { contains: constraints.search };
+            });
+
+            prismaConditions.where.OR.push(relationshipConstraint);
+        });
+    }
+
+    if (constraints.filter) {
+        prismaConditions.where = {};
+        convertFilterClauseToPrismaClause(constraints.filter, prismaConditions.where);
+    }
+
+    return prismaConditions;
+};
+
+const convertFilterClauseToPrismaClause = (filterConstraint = {}, prismaFilterConditions = {}) => {
+    Object.keys(filterConstraint).forEach((filterValue) => {
+        prismaFilterConditions[filterValue] = {};
+
+        const depth = getObjectDepth(filterConstraint[filterValue]);
+        if (depth > 1) {
+            console.log("DEPTH 1");
+            // Nested relationship
+            convertFilterClauseToPrismaClause(filterConstraint[filterValue], prismaFilterConditions[filterValue], filterValue);
+            return;
+        }
+
+        Object.keys(filterConstraint[filterValue]).forEach((condition) => {
+            let prismaCondition = condition;
+
+            if (condition === "eq") prismaCondition = "equals";
+            if (condition === "ne") prismaCondition = "not";
+            if (condition === "like") prismaCondition = "contains";
+
+            prismaFilterConditions[filterValue][prismaCondition] = filterConstraint[filterValue][condition];
+        });
+    });
+};
+
+/**
+ * Returns the integer value of the depth of the provided object
+ * @param {Object} objectToCheck NOTE: Recursive object will BREAK
+ * TODO: Add to dx-utilities
+ * @returns {number}
+ */
+const getObjectDepth = (objectToCheck) => {
+    return Object(objectToCheck) === objectToCheck ? 1 + Math.max(-1, ...Object.values(objectToCheck).map(getObjectDepth)) : 0;
+};
