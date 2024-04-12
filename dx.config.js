d2a07f1aa88d7b2cdc376866ea0fa80490760e84
Dani <dani.simeonov@stratusolve.com>
2024-04-12 11:55:16 +0200

    Minor update

--- dx.config.js
+++ dx.config.js
@@ -5,7 +5,7 @@ export default {
     environment: "local", // Current environment,
     dataModelPath: "divblox/configs/datamodel.json", // Path from route to the data model JSON file
     databaseConfigPath: "divblox/configs/database.config.js", // Path from root to the database configuration file
-    databaseCaseImplementation: "snakecase", // Allowed options ['snakecase'|'camelcase'|'pascalcase']
+    databaseCaseImplementation: "camelcase", // Allowed options ['snakecase'|'camelcase'|'pascalcase']
     ormImplementation: "prisma", // Supported options ['none','prisma']; If defined, the relevant orm will automatically be installed and used for things like component generation
     codeGen: {
         dataModelUiConfigPath: "divblox/code-gen/datamodel-ui.config.json", // Path from route to the data model UI configuration file
