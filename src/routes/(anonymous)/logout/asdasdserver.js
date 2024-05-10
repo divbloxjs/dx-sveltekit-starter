a7a879ea7038b7acbc9c9dd8dfd3d4f05cca276d
Dani <dani.simeonov@stratusolve.com>
2024-05-10 10:29:51 +0200

    Progress

--- src/routes/(anonymous)/logout/asdasdserver.js
+++
@@ -1,7 +0,0 @@
-import { json, redirect } from "@sveltejs/kit";
-
-/** @type {import('./$types').RequestHandler} */
-export async function POST({ cookies }) {
-    cookies.delete("sessionId", { path: "/" });
-    redirect(307, "/login");
-}
