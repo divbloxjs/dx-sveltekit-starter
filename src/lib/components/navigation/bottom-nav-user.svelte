1952b6a91283cb18e6b9e25d01bac1efd2378029
Johan Griesel <johan@stratusolve.com>
2024-06-11 11:26:12 +0200

    Better safe areas

--- src/lib/components/navigation/bottom-nav-user.svelte
+++ src/lib/components/navigation/bottom-nav-user.svelte
@@ -19,7 +19,8 @@
 
 <form bind:this={logoutForm} action="/api/logout" method="POST"></form>
 
-<div class="fixed bottom-0 z-50 flex h-[calc(env(safe-area-inset-bottom)+3.5rem)] w-full items-center justify-between bg-slate-200 px-2">
+<div
+    class="fixed bottom-0 z-50 flex h-[calc(env(safe-area-inset-bottom)+3.5rem)] w-full items-start justify-between bg-slate-200 px-2 pb-[env(safe-area-inset-bottom)] pt-2">
     <Button
         type="button"
         variant="link"
