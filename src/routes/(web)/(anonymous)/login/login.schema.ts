c1264f19c073c69645a779b44b06e1f19421fca0
Dani <dani.simeonov@stratusolve.com>
2024-06-11 12:01:21 +0200

    Progress

--- src/routes/(web)/(anonymous)/login/login.schema.ts
+++ src/routes/(web)/(anonymous)/login/login.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
     emailAddress: z.string().email().max(150),
-    password: z.string().min(8)
+    password: z.string().min(1, { message: "Required" })
 });
 
 export type LoginSchema = typeof loginSchema;
