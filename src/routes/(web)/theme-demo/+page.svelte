cee40c54f5c2fb32651637c585b4fe8cc4f5dca9
Dani <dani.simeonov@stratusolve.com>
2024-06-18 15:51:05 +0200

    Removed old theme demo

--- src/routes/(web)/theme-demo/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Theme from "$lib/components/demo/theme.svelte";
-
-    export let data;
-</script>
-
-<div class="flex h-full w-full flex-col">
-    <Theme></Theme>
-</div>
