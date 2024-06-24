1eb8a91ddbe10dfd25f6102e673161aa9d2949ee
Dani <dani.simeonov@stratusolve.com>
2024-06-24 08:32:27 +0200

    removing unneeded routes

--- src/routes/(web)/user-session/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import CrudUserSession from "$lib/components/data-model/user-session/user-session-crud.svelte";
-
-    export let data;
-</script>
-
-<CrudUserSession {data} basePath="/user-session" />
