5935b7e526d4e18ee17f36dabaf30886b1c5403c
Dani Simeonov <dani.simeonov@stratusolve.com>
2024-11-20 14:32:11 +0200

    Updated filters

--- src/lib/components/data-model/_partial-components/data-series/filter-inputs/filter-enum.svelte
+++ src/lib/components/data-model/_partial-components/data-series/filter-inputs/filter-enum.svelte
@@ -1,5 +1,8 @@
 <script>
+    import Button from "$components/shadcn/ui/button/button.svelte";
+    import Input from "$components/shadcn/ui/input/input.svelte";
     import * as Select from "$lib/components/shadcn/ui/select/index";
+    import { RotateCcw } from "lucide-svelte";
     import { createEventDispatcher } from "svelte";
 
     export let placeholder = "-Please Select-";
@@ -11,28 +14,38 @@
 
     const dispatch = createEventDispatcher();
 
+    const handleFilterClear = () => {
+        selectedValue = "";
+        dispatch("filter-clear");
+    };
+
     let selected;
 
     $: selectedValue, (selected = options.find(({ value, label }) => value === selectedValue) ?? { label: "-Please Select-", value: "" });
 </script>
 
-<Select.Root
-    {selected}
-    onSelectedChange={(v) => {
-        v && (selectedValue = v.value);
-        dispatch("filter-change", { filterValue: selectedValue, filterComparison: "eq" });
-    }}>
-    <Select.Input {name} />
-    <Select.Trigger>
-        <Select.Value {placeholder} />
-    </Select.Trigger>
-    <Select.Content>
-        {#if options.length === 0}
-            <Select.Item disabled value="" label="No options" />
-        {/if}
+<div class="flex items-center gap-1">
+    <Select.Root
+        {selected}
+        onSelectedChange={(v) => {
+            v && (selectedValue = v.value);
+            dispatch("filter-change", { filterValue: selectedValue, filterComparison: "eq" });
+        }}>
+        <Select.Input {name} />
+        <Select.Trigger class="h-6 min-w-40">
+            <Select.Value {placeholder} />
+        </Select.Trigger>
+        <Select.Content>
+            {#if options.length === 0}
+                <Select.Item disabled value="" label="No options" />
+            {/if}
 
-        {#each options as option}
-            <Select.Item value={option.value} label={option.label} />
-        {/each}
-    </Select.Content>
-</Select.Root>
+            {#each options as option}
+                <Select.Item value={option.value} label={option.label} />
+            {/each}
+        </Select.Content>
+    </Select.Root>
+    <Button variant="link" size="inline-icon" on:click={handleFilterClear}>
+        <RotateCcw size="18" />
+    </Button>
+</div>
