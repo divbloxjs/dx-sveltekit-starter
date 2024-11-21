ab78dc829debb23f3b4f3f12e53739c54c4e4bbb
Dani Simeonov <dani.simeonov@stratusolve.com>
2024-11-21 20:10:10 +0200

    Clean up

--- src/routes/(web)/funding-event/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import CrudFundingEvent from "$lib/components/data-model/funding-event/funding-event-crud.svelte";
-
-    export let data;
-</script>
-
-<CrudFundingEvent {data} basePath="/funding-event" />
